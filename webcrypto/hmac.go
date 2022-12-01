6f83022244c165946a271bea2ef1940b21e82dc3
oleiade <theo@crevon.me>
2022-12-01 16:51:54 +0100

    Implement support for HMAC key import

--- webcrypto/hmac.go
+++ webcrypto/hmac.go
@@ -3,6 +3,7 @@ package webcrypto
 import (
 	"crypto/hmac"
 	"crypto/rand"
+	"encoding/base64"
 	"strings"
 
 	"github.com/dop251/goja"
@@ -201,4 +202,234 @@ func (h HmacKeyGenParams) GenerateKey(
 
 	// 14.
 	return &key, nil
+}
+
+// HmacImportParams represents the HMAC key algorithm parameters
+// used to import a HMAC key.
+type HmacImportParams struct {
+	Algorithm
+
+	// The inner hash function to use.
+	Hash string `json:"hash"`
+
+	// The length of the key in bits.
+	Length *uint32 `json:"length"`
+}
+
+// NewHmacImportParams creates a new HmacImportParams instance from the given goja.Value.
+func NewHmacImportParams(rt *goja.Runtime, v goja.Value) (HmacImportParams, error) {
+	if v == nil {
+		return HmacImportParams{}, NewError(0, SyntaxError, "HmacImportParams is required")
+	}
+
+	var params HmacImportParams
+	if err := rt.ExportTo(v, &params); err != nil {
+		return HmacImportParams{}, NewError(0, SyntaxError, "unable to interpret HmacImportParams argument")
+	}
+
+	hashValue := v.ToObject(rt).Get("hash")
+	if hashValue == nil {
+		return HmacImportParams{}, NewError(0, SyntaxError, "hash property is required")
+	}
+
+	hash, ok := hashValue.Export().(string)
+	if !ok {
+		return HmacImportParams{}, NewError(0, SyntaxError, "hash property must be a string")
+	}
+
+	params.Hash = strings.ToUpper(hash)
+
+	lengthValue := v.ToObject(rt).Get("length")
+	if lengthValue != nil {
+		length, ok := lengthValue.Export().(*uint32)
+		if !ok {
+			return HmacImportParams{}, NewError(0, SyntaxError, "length property must be a number")
+		}
+
+		params.Length = length
+	}
+
+	return params, nil
+}
+
+// importHMACKey imports a HMAC key from the given key data and turns it
+// into a CryptoKey.
+//
+// TODO: Note that AES-KW is not supported as of yet.
+func importHMACKey(
+	rt *goja.Runtime,
+	normalizedAlgorithm HmacImportParams,
+	keyData goja.Value,
+	format KeyFormat,
+	extractable bool,
+	usages []CryptoKeyUsage,
+) (CryptoKey[[]byte], error) {
+	// 2.
+	if !ContainsOnly(usages, SignCryptoKeyUsage, VerifyCryptoKeyUsage) {
+		return CryptoKey[[]byte]{}, NewError(0, SyntaxError, "invalid key usage")
+	}
+
+	// 3.
+	hash := KeyAlgorithm{}
+
+	// 4.
+	var data []byte
+	var err error
+
+	switch format {
+	case RawKeyFormat:
+		// 4.1. 4.2.
+		data, err = importHMACKeyFromRaw(rt, keyData, normalizedAlgorithm, &hash)
+		if err != nil {
+			return CryptoKey[[]byte]{}, err
+		}
+	case JwkKeyFormat:
+		// 4.1. 4.2.
+		data, err = importHMACKeyFromJwk(rt, keyData, extractable, usages, normalizedAlgorithm, &hash)
+		if err != nil {
+			return CryptoKey[[]byte]{}, err
+		}
+	default:
+		return CryptoKey[[]byte]{}, NewError(0, NotSupportedError, "unsupported key format")
+	}
+
+	// 5.
+	length := uint32(len(data) * 8)
+
+	// 6.
+	if length == 0 {
+		return CryptoKey[[]byte]{}, NewError(0, DataError, "the key data is empty")
+	}
+
+	// 7.
+	switch nl := normalizedAlgorithm.Length; {
+	case nl != nil && *nl > length:
+		return CryptoKey[[]byte]{}, NewError(0, DataError, "the key data length and import params' do not match")
+	case nl != nil && *nl <= length-8:
+		return CryptoKey[[]byte]{}, NewError(0, DataError, "the key data length and import params' do not match")
+	default:
+		length = *normalizedAlgorithm.Length
+	}
+
+	// 8.
+	key := CryptoKey[[]byte]{
+		handle: data[:length/8],
+	}
+
+	// 9.
+	algorithm := HmacKeyAlgorithm{}
+
+	// 10.
+	algorithm.Name = HMAC
+
+	// 11.
+	algorithm.Length = length
+
+	// 12.
+	algorithm.Hash = hash
+
+	// 13.
+	key.Algorithm = algorithm
+
+	return key, nil
+}
+
+func importHMACKeyFromRaw(
+	rt *goja.Runtime,
+	keyData goja.Value,
+	normalizedAlgorithm HmacImportParams,
+	hash *KeyAlgorithm,
+) ([]byte, error) {
+	var data []byte
+
+	// 4.1.
+	err := rt.ExportTo(keyData, &data)
+	if err != nil {
+		return nil, NewError(0, DataError, "invalid key data")
+	}
+
+	// 4.2.
+	hash.Name = normalizedAlgorithm.Hash
+
+	return data, nil
+}
+
+//nolint:gocognit,cyclop
+func importHMACKeyFromJwk(
+	rt *goja.Runtime,
+	keyData goja.Value,
+	extractable bool,
+	usages []CryptoKeyUsage,
+	normalizedAlgorithm HmacImportParams,
+	hash *KeyAlgorithm,
+) ([]byte, error) {
+	var data []byte
+
+	// 2.1.
+	var jwk JSONWebKey
+	err := rt.ExportTo(keyData, &jwk)
+	if err != nil {
+		return nil, NewError(0, DataError, "could not import data as JSON Web Key")
+	}
+
+	// 2.2.
+	if jwk.KeyType != "oct" {
+		return nil, NewError(0, DataError, "invalid key type")
+	}
+
+	// 2.3.
+	if jwk.K == "" || jwk.Algorithm == "" {
+		return nil, NewError(0, DataError, "invalid key data")
+	}
+
+	// 2.4.
+	data, err = base64.URLEncoding.DecodeString(jwk.K)
+	if err != nil {
+		return nil, NewError(0, DataError, "invalid decoded key data")
+	}
+
+	// 2.5.
+	hash.Name = normalizedAlgorithm.Name
+
+	// 2.6.
+	switch hash.Name {
+	case Sha1:
+		if jwk.Algorithm != "" && jwk.Algorithm != "HS1" {
+			return nil, NewError(0, DataError, "invalid algorithm")
+		}
+	case Sha256:
+		if jwk.Algorithm != "" && jwk.Algorithm != "HS256" {
+			return nil, NewError(0, DataError, "invalid algorithm")
+		}
+	case Sha384:
+		if jwk.Algorithm != "" && jwk.Algorithm != "HS384" {
+			return nil, NewError(0, DataError, "invalid algorithm")
+		}
+	case Sha512:
+		if jwk.Algorithm != "" && jwk.Algorithm != "HS512" {
+			return nil, NewError(0, DataError, "invalid algorithm")
+		}
+	default:
+		return nil, NewError(0, DataError, "invalid algorithm")
+	}
+
+	// 2.7.
+	if len(usages) != 0 && jwk.Use != "" && jwk.Use != "sign" {
+		return nil, NewError(0, DataError, "invalid key usage")
+	}
+
+	// 2.8.
+	if jwk.KeyOps != nil {
+		for _, usage := range usages {
+			if !Contains(jwk.KeyOps, usage) {
+				return nil, NewError(0, DataError, "invalid key usage")
+			}
+		}
+	}
+
+	if !jwk.Extractable && extractable {
+		return nil, NewError(0, DataError, "invalid key extractability")
+	}
+
+	return data, nil
 }
