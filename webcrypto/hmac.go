17ca412d2adcd2c9f93f0f2df4c53dd4ff2f53f9
oleiade <theo@crevon.me>
2022-12-01 16:28:56 +0100

    Implement support for HMAC key export

--- webcrypto/hmac.go
+++ webcrypto/hmac.go
@@ -3,6 +3,7 @@ package webcrypto
 import (
 	"crypto/hmac"
 	"crypto/rand"
+	"encoding/base64"
 	"fmt"
 	"strings"
 
@@ -44,12 +45,10 @@ var _ From[map[string]interface{}, HmacKeyGenParams] = HmacKeyGenParams{}
 
 // From implements the From interface for HmacKeyGenParams, and initializes the
 // instance from a map[string]interface{}.
-//
-//nolint:ifshort
 func (h HmacKeyGenParams) From(dict map[string]interface{}) (HmacKeyGenParams, error) {
 	params := HmacKeyGenParams{}
 	nameFound := false
-	hashFound := false
+	hashFound := false //nolint:ifshort
 
 	for key, value := range dict {
 		if strings.EqualFold(key, "name") {
@@ -209,4 +208,77 @@ func (h *HmacKeyGenParams) GenerateKey(
 
 	// 14.
 	return rt.ToValue(key), nil
+}
+
+// exportHMACKey exports a HMAC key in the given format.
+// As defined in the [specification] for exporting HMAC keys
+//
+// [specification]: https://www.w3.org/TR/WebCryptoAPI/#hmac-operations
+func exportHMACKey(rt *goja.Runtime, format KeyFormat, key CryptoKey[[]byte]) (goja.Value, error) {
+	// 1.
+	if key.handle == nil {
+		return nil, NewError(0, OperationError, "the key is not valid, no data")
+	}
+
+	// 2. 3.
+	// TODO: verify that we comply with the spec here (octet string): https://www.w3.org/TR/WebCryptoAPI/#hmac-operations
+	data := key.handle
+
+	switch format {
+	case RawKeyFormat:
+		// 4.1.
+		return rt.ToValue(rt.NewArrayBuffer(data)), nil
+	case JwkKeyFormat:
+		return exportHMACKeyAsJwk(rt, key)
+	default:
+		return nil, NewError(0, NotSupportedError, "unsupported key format")
+	}
+}
+
+// exportHMACKeyAsJwk exports a HMAC key as a JWK object.
+// As defined in the [specification] for exporting HMAC keys
+//
+// [specification]: https://www.w3.org/TR/WebCryptoAPI/#hmac-operations
+func exportHMACKeyAsJwk(rt *goja.Runtime, key CryptoKey[[]byte]) (goja.Value, error) {
+	// 4.1.
+	jwk := JSONWebKey{}
+
+	// 4.2.
+	jwk.KeyType = "oct"
+
+	// 4.3.
+	jwk.K = base64.URLEncoding.EncodeToString(key.handle)
+
+	// 4.4.
+	algorithm, ok := key.Algorithm.(HmacKeyAlgorithm)
+	if !ok {
+		return nil, NewError(0, ImplementationError, "unable to extract key's algorithm")
+	}
+
+	// 4.5.
+	hash := algorithm.Hash
+
+	// 4.6.
+	switch hash.Name {
+	case Sha1:
+		jwk.Algorithm = "HS1"
+	case Sha256:
+		jwk.Algorithm = "HS256"
+	case Sha384:
+		jwk.Algorithm = "HS384"
+	case Sha512:
+		jwk.Algorithm = "HS512"
+	default:
+		return nil, NewError(0, OperationError, "unsupported hash algorithm")
+	}
+
+	// 4.7.
+	jwk.KeyOps = make([]string, len(key.Usages))
+	copy(jwk.KeyOps, key.Usages)
+
+	// 4.8.
+	jwk.Extractable = key.Extractable
+
+	// 4.9.
+	return rt.ToValue(jwk), nil
 }
