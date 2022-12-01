ee4a59a510d613d12fe2d7cfdef0ffe6bf75fb6b
oleiade <theo@crevon.me>
2022-12-01 16:50:47 +0100

    Implement support AES key import

--- webcrypto/aes.go
+++ webcrypto/aes.go
@@ -2,6 +2,7 @@ package webcrypto
 
 import (
 	"crypto/rand"
+	"encoding/base64"
 	"strings"
 
 	"github.com/dop251/goja"
@@ -156,4 +157,181 @@ func (a AesKeyGenParams) GenerateKey(
 
 	// 12.
 	return &key, nil
+}
+
+// importAES imports an AES key from the given key data and turns it
+// into a CryptoKey.
+// The key data must be a base64 encoded string. The key data
+// must be a valid AES key.
+//
+// TODO: Note that AES-KW is not supported as of yet.
+func importAESKey(
+	rt *goja.Runtime,
+	algorithmName AlgorithmIdentifier,
+	keyData goja.Value,
+	format KeyFormat,
+	extractable bool,
+	usages []CryptoKeyUsage,
+) (CryptoKey[[]byte], error) {
+	// 1.
+	if !ContainsOnly(
+		usages,
+		EncryptCryptoKeyUsage,
+		DecryptCryptoKeyUsage,
+		WrapKeyCryptoKeyUsage,
+		UnwrapKeyCryptoKeyUsage) {
+		return CryptoKey[[]byte]{}, NewError(0, SyntaxError, "invalid key usage")
+	}
+
+	// 2.
+	var data []byte
+	var err error
+
+	switch format {
+	case RawKeyFormat:
+		data, err = importAESKeyFromRaw(rt, keyData)
+		if err != nil {
+			return CryptoKey[[]byte]{}, err
+		}
+	case JwkKeyFormat:
+		data, err = importAESKeyFromJwk(rt, algorithmName, keyData, extractable, usages)
+		if err != nil {
+			return CryptoKey[[]byte]{}, err
+		}
+	default:
+		return CryptoKey[[]byte]{}, NewError(0, NotSupportedError, "unsupported key format")
+	}
+
+	// 3.
+	key := CryptoKey[[]byte]{
+		Type:   SecretCryptoKeyType,
+		handle: data,
+	}
+
+	// 4.
+	algorithm := AesKeyAlgorithm{}
+
+	// 5.
+	algorithm.Name = algorithmName
+
+	// 6.
+	algorithm.Length = int(KeyLength(len(data) * 8)) // length is in bits
+
+	// 7.
+	key.Algorithm = algorithm
+
+	// 8.
+	return key, nil
+}
+
+func importAESKeyFromRaw(rt *goja.Runtime, keyData goja.Value) ([]byte, error) {
+	var data []byte
+
+	// 2.1.
+	err := rt.ExportTo(keyData, &data)
+	if err != nil {
+		return nil, NewError(0, DataError, "could not export key data")
+	}
+
+	// 2.2.
+	var (
+		has128Bits = len(data) == 16
+		has192Bits = len(data) == 24
+		has256Bits = len(data) == 32
+	)
+
+	if !has128Bits && !has192Bits && has256Bits {
+		return nil, NewError(0, DataError, "invalid key length")
+	}
+
+	return data, nil
+}
+
+// importAESKeyAsJwk imports an AES key from the given JWK key data and
+// returns a CryptoKey handle bytes.
+//
+//nolint:funlen,gocognit,cyclop
+func importAESKeyFromJwk(
+	rt *goja.Runtime,
+	algorithmName AlgorithmIdentifier,
+	keyData goja.Value,
+	extractable bool,
+	usages []CryptoKeyUsage,
+) ([]byte, error) {
+	var data []byte
+
+	// 2.1.
+	var jwk JSONWebKey
+	err := rt.ExportTo(keyData, &jwk)
+	if err != nil {
+		return nil, NewError(0, DataError, "could not import data as JSON Web Key")
+	}
+
+	// 2.2.
+	if jwk.KeyType != "oct" {
+		return nil, NewError(0, DataError, "invalid key type")
+	}
+
+	// 2.3.
+	if jwk.K == "" || jwk.Algorithm == "" {
+		return nil, NewError(0, DataError, "invalid key data")
+	}
+
+	// 2.4.
+	data, err = base64.URLEncoding.DecodeString(jwk.K)
+	if err != nil {
+		return nil, NewError(0, DataError, "invalid decoded key data")
+	}
+
+	// 2.5.
+	var suffix string
+
+	switch algorithmName {
+	case AESCbc:
+		suffix = "CBC"
+	case AESCtr:
+		suffix = "CTR"
+	case AESGcm:
+		suffix = "GCM"
+	default:
+		return nil, NewError(0, DataError, "invalid algorithm")
+	}
+
+	switch len(data) {
+	case 16: // 128 bits
+		if jwk.Algorithm != "" && jwk.Algorithm != "A128"+suffix {
+			return nil, NewError(0, DataError, "invalid algorithm")
+		}
+	case 24: // 192 bits
+		if jwk.Algorithm != "" && jwk.Algorithm != "A192"+suffix {
+			return nil, NewError(0, DataError, "invalid algorithm")
+		}
+	case 32: // 256 bits
+		if jwk.Algorithm != "" && jwk.Algorithm != "A256"+suffix {
+			return nil, NewError(0, DataError, "invalid algorithm")
+		}
+	default:
+		return nil, NewError(0, DataError, "invalid key length")
+	}
+
+	// 2.6.
+	if len(usages) != 0 && jwk.Use != "" && jwk.Use != "enc" {
+		return nil, NewError(0, DataError, "invalid key usage")
+	}
+
+	// 2.7.
+	if jwk.KeyOps != nil {
+		for _, usage := range usages {
+			if !Contains(jwk.KeyOps, usage) {
+				return nil, NewError(0, DataError, "invalid key usage")
+			}
+		}
+	}
+
+	// 2.8.
+	if !jwk.Extractable && extractable {
+		return nil, NewError(0, DataError, "invalid key extractability")
+	}
+
+	return data, nil
 }
