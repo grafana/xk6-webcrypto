bc625655f3314fc2d2e83395d8ff69f35c56752b
Lucas Pearson <catdevman@gmail.com>
2024-10-02 16:57:47 -0400

    detect new key type and handle creating a KeyImportParam for PBKDF2

--- webcrypto/pbkdf2.go
+++ webcrypto/pbkdf2.go
@@ -21,6 +21,23 @@ func newPBKDF2KeyDeriveParams(rt *sobek.Runtime, normalized Algorithm, params so
 	}, nil
 }
 
-func (p PBKDF2Params) DeriveKey() (CryptoKeyGenerationResult, error){
+func (p PBKDF2Params) DeriveKey() (CryptoKeyGenerationResult, error) {
 	return nil, nil
 }
+
+// EcKeyImportParams represents the object that should be passed as the algorithm parameter
+// into `SubtleCrypto.ImportKey` or `SubtleCrypto.UnwrapKey`, when generating any elliptic-curve-based
+// key pair: that is, when the algorithm is identified as either of ECDSA or ECDH.
+type PBKDF2KeyImportParams struct {
+	Algorithm
+}
+
+func newPBKDF2KeyImportParams(rt *sobek.Runtime, normalized Algorithm, params sobek.Value) (*PBKDF2KeyImportParams, error) {
+
+	return &PBKDF2KeyImportParams{
+		Algorithm: normalized,
+	}, nil
+}
+
+// Ensure that EcKeyImportParams implements the KeyImporter interface.
+var _ KeyImporter = &PBKDF2KeyImportParams{}
