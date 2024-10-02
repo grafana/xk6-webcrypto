c692850d566244c211aae2feb3595a7469277ff1
Lucas Pearson <catdevman@gmail.com>
2024-10-02 16:56:19 -0400

    I think this a good test... I took the example from MDN :shrug:

--- examples/derive_key/derive-key-pbkdf2.js
+++ examples/derive_key/derive-key-pbkdf2.js
@@ -1,7 +1,24 @@
 import { crypto } from "k6/x/webcrypto";
 
-export default async function () {
-  const key = await crypto.subtle.deriveKey();
-
+export default async function() {
+  const keyMaterial = crypto.subtle.importKey(
+    "raw",
+    new Uint8Array([65, 66, 67]),
+    "PBKDF2",
+    false,
+    ["deriveBits", "deriveKey"],
+  );
+  const key = await crypto.subtle.deriveKey(
+    {
+      name: "PBKDF2",
+      salt,
+      iterations: 100000,
+      hash: "SHA-256",
+    },
+    keyMaterial,
+    { name: "AES-GCM", length: 256 },
+    true,
+    ["encrypt", "decrypt"],
+  )
   console.log(JSON.stringify(key));
 }
